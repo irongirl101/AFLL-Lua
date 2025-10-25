4d55e011767b61de5e8f9833766aa2d286a22a9e
irongirl101 <aditi061806@gmail.com>
2025-10-25 18:49:52 +0530

    idk

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
 import ply.lex as lex 
 import ply.yacc as yacc
 
-# List of token names. This is always required
-tokens = ('A', 'B')
+# ok for this, as a comment could have hyphens in the comment itself, i need to have two different grammars for both halves. 
+# one for the double hyphen, one for the rest of the content, it being anything. 
 
-# Regular expression rules for simple tokens
-t_A = r'a'
-t_B = r'b'
-# Error handling rule
-def t_error(t):
-    print(f"Illegal character '{t.value[0]}'")
+#states 
+states = (('content','exclusive'))
+
+
+tokens = ('DOUBLE_HYPHEN','CONTENT')
+
+def t_INITIAL_DOUBLE_HYPHEN(t):
+    r'--'
+    t.type = 'DOUBLE_HYPHEN'
+    t.lexer.begin('content')
+    return t
+
+def t_INITIAL_error(t):
+    print(f"[INITIAL] Illegal character '{t.value[0]}' on line {t.lexer.lineno}")
     t.lexer.skip(1)
 
-# Building the lexer
+def t_content_CONTENT(t):
+    r'.+'
+    t.type = 'CONTENT'
+    t.lexer.begin('INITIAL')
+    return t
+
+def t_content_error(t):
+    print(f"[content] Illegal character '{t.value[0]}' on line {t.lexer.lineno}")
+    t.lexer.skip(1)
+
 lexer = lex.lex()
 
-def p_sequence(p):
-    '''S : A S B
-    | A B '''
+def p_sequence(p): 
+    '''S : DOUBLE_HYPHEN CONTENT'''
+    p[0] = p[2]
 
-# Error rule for syntax errors
+
 def p_error(p):
-    print("Syntax error")
-    exit(1)
-# Build the parser
+    if p:
+        print(f"Syntax error at token {p.type} ('{p.value}')")
+    else:
+        print("Syntax error at EOF")
+
 parser = yacc.yacc()
 
 while True:
-    data = input("Enter a^n b^n sequence (e.g., 'aaabbb'): ")
-    lexer.input(data)
-    parsed = parser.parse(data)
-    if parsed == None:
+    try:
+        data = input("Enter Single Line: ")
+    except EOFError:
+        print("\nExiting.")
+        break
+    
+    parsed = parser.parse(data, lexer=lexer)
+    if parsed is not None:
         print("Accepted")
